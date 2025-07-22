cdcc64da3f6739f41c3ae654e2c4202e68d1e5e5
Tresor Diwata <dev@tresordiwata.com>
2025-07-22 11:46:30 +0200

    Build 6

--- app/(dashboard)/gestion-client/page.tsx
+++ app/(dashboard)/gestion-client/page.tsx
@@ -1,7 +1,8 @@
 import { NextRequest } from "next/server";
 import PageClient from "./PageClient";
 
-export default async function Home(Req:NextRequest) {
+// export default async function Home(Req:NextRequest) {
+export default async function Home() {
   
   return <PageClient />;
 }
